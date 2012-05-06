6ee31dc54d68083ceb320b36005cc5caab48939a
Elijah Rutschman <elijahr@gmail.com>
2012-05-06 12:24:19 -0500

    include package data so static files and templates are available

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -11,13 +11,20 @@ with open('LICENSE') as f:
 
 setup(
     name='django-orderable-inlines',
-    version='0.0.1',
-    description='Draggable TabularInlines and StackedInlines for Django',
+    version='0.0.5',
+    description='Drag-to-reorder inline model admins for Django',
     long_description=readme,
     author='Elijah Rutschman',
     author_email='elijahr+django-orderable-inlines@gmail.com',
     url='https://github.com/elijahr/django-orderable-inlines',
     license=license,
-    packages=find_packages(exclude=('tests',)),
-    package_data={ 'orderable_inlines': ['static/*'], },
+    packages=find_packages(),
+    package_data={
+        'orderable_inlines': [
+            'static/css/*',
+            'static/js/*',
+            'templates/orderable_inlines/edit_inline/*',
+        ],
+    },
+    include_package_data=True,
 )
