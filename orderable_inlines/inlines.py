a579aecc39eeeb22f14c7a154f3a11c487774658
Tim Kamanin <tim@timonweb.com>
2014-08-19 16:42:13 +0200

    Fixed inlines (jquery ui and fieldset) to make them work with Django 1.6

--- orderable_inlines/inlines.py
+++ orderable_inlines/inlines.py
@@ -14,11 +14,23 @@ class OrderableInlineMixin(object):
             ]
         }
 
-    def get_fieldsets(self, request, obj=None):
+    def get_fieldsets2(self, request, obj=None):
         if self.declared_fieldsets:
             return self.declared_fieldsets
         form = self.get_formset(request, obj).form
         fields = form.base_fields.keys() + list(self.get_readonly_fields(request, obj))
+        return [
+            (None, {
+                'fields': fields,
+                'classes': self.fieldset_css_classes + ['orderable-field-%s' % self.orderable_field]
+            })
+        ]
+
+    def get_fieldsets(self, request, obj=None):
+        if self.declared_fieldsets:
+            return self.declared_fieldsets
+        form = self.get_formset(request, obj, fields=None).form
+        fields = list(form.base_fields) + list(self.get_readonly_fields(request, obj))
         return [
             (None, {
                 'fields': fields,
